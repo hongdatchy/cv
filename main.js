bb5e6304e421e9ff86c8bef29498c9d850bfbcdc
hongdatchy <hongdatchy@gmail.com>
2022-01-23 21:48:56 +0700

    test commit on vs code

--- main.js
+++ main.js
@@ -6,46 +6,39 @@ $(document).ready(function(){
     });
     
 	my_section_arr=[$('#home'),$('#about'), $('#skills'), $('#experiences'), $('#education'), $('#end')];
+
 	for(let i = 0; i<5;i++){
 		$('.content-natigation li:nth-child('+(i+1)+')').on('click', function(event) {
 			event.preventDefault();
 			$('html').animate({
-				scrollTop:my_section_arr[i].offset().top+5},800,"easeOutSine");
+				scrollTop:my_section_arr[i].offset().top+5},600,"easeInOutCubic");
 				// cộng 5 để cho nó tụt xuống 1 cái section 1 tý, để cái dưới chạy đỡ sai
 		});
 	}
     
-    
+    active_section();
     $(window).scroll(function(event) {
-		for(let i = 0; i<5; i++){
-			if(my_section_arr[i].offset().top<=$('html').scrollTop() && $('html').scrollTop() <my_section_arr[i+1].offset().top){
-				x=i+1;
-				console.log(my_section_arr[i+1].offset());
-			console.log($('html').scrollTop());
-			}
-			
+		active_section();
+	});
+});
+
+function active_section() {
+	for(let i = 0; i<5; i++){
+		if(my_section_arr[i].offset().top<=$('html').scrollTop() && $('html').scrollTop() <my_section_arr[i+1].offset().top){
+			x=i+1;
 		}
 		
-		$('.content-natigation li:nth-child('+(x)+')').addClass
-			('active');
-			$('.content-natigation li:nth-child('+(x-1)+')').removeClass
-		('active');
-			$('.content-natigation li:nth-child('+(x+1)+')').removeClass
-		('active');
-// 
-		$('.content-natigation li:nth-child('+(x)+') i').addClass
-			('active');
-			$('.content-natigation li:nth-child('+(x-1)+') i').removeClass
-		('active');
-			$('.content-natigation li:nth-child('+(x+1)+') i').removeClass
-		('active');
-// 
-		$('.content-natigation li:nth-child('+(x)+') span').addClass
-			('active');
-			$('.content-natigation li:nth-child('+(x-1)+') span').removeClass
-		('active');
-			$('.content-natigation li:nth-child('+(x+1)+') span').removeClass
-		('active');
+	}
 	
-	});;
-});
+	$('.content-natigation li:nth-child('+(x)+')').addClass('active');
+	$('.content-natigation li:nth-child('+(x-1)+')').removeClass('active');
+	$('.content-natigation li:nth-child('+(x+1)+')').removeClass('active');
+// 
+	$('.content-natigation li:nth-child('+(x)+') i').addClass('active');
+	$('.content-natigation li:nth-child('+(x-1)+') i').removeClass('active');
+	$('.content-natigation li:nth-child('+(x+1)+') i').removeClass('active');
+// 
+	$('.content-natigation li:nth-child('+(x)+') span').addClass('active');
+	$('.content-natigation li:nth-child('+(x-1)+') span').removeClass('active');
+	$('.content-natigation li:nth-child('+(x+1)+') span').removeClass('active');
+}
